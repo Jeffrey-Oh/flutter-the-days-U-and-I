e8a0af3f68fe8ac1f48c8c2565d9ae85cc71bfaa
오태현 <jeffrey@kai-i.com>
2022-06-12 15:29:19 +0900

    디자인 마무리하기

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
  - [x] Asset 추가하기
  - [x] TextStyle 설정하기
- - [ ] 디자인 마무리
+ - [x] 디자인 마무리
  - [ ] DatePicker 사용하기
  - [ ] DateTime 상태관리
  - [ ] 상태를 트리의 위로
--- lib/screen/home/HomeScreen.dart
+++ lib/screen/home/HomeScreen.dart
@@ -13,18 +13,40 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.pink[100],
       body: SafeArea(
+        bottom: false,
         child: Container(
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              Text(
-                'U&I',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'parisienne',
-                  fontSize: 80,
-                ),
-              ),
+              _TopWidget(),
+              _BottomWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TopWidget extends StatelessWidget {
+  const _TopWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'U&I',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'parisienne',
+              fontSize: 80,
+            ),
+          ),
+          Column(
+            children: [
               Text(
                 '우리 처음 만난 날',
                 style: TextStyle(
@@ -35,21 +57,38 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: TextStyle(
                     color: Colors.white, fontFamily: 'sunflower', fontSize: 20),
               ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.favorite,
-                  color: Colors.red,
-                ),
-              ),
-              Text(
-                'D+1',
-                style: TextStyle(
-                    color: Colors.white, fontFamily: 'sunflower', fontSize: 50, fontWeight: FontWeight.w700),
-              ),
             ],
           ),
-        ),
+          IconButton(
+            iconSize: 60,
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+            ),
+          ),
+          Text(
+            'D+1',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'sunflower',
+                fontSize: 50,
+                fontWeight: FontWeight.w700),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomWidget extends StatelessWidget {
+  const _BottomWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset(
+        'assets/img/background.png',
       ),
     );
   }
